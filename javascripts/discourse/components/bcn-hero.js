07b116cf49a2526ec04ba5edf7503bec1c71107b
Andrew Dickens <andrewcodes404@gmail.com>
2024-05-22 10:07:48 +0100

    add default hero to topic pages

--- javascripts/discourse/components/bcn-hero.js
+++ javascripts/discourse/components/bcn-hero.js
@@ -2,35 +2,30 @@ import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
 import { defaultHomepage } from "discourse/lib/utilities";
 
-
 export default class BcnHero extends Component {
   @service router;
   @service discovery;
 
+  
   get showHero() {
-    const { session, currentRoute, currentRouteName, location, currentURL, topicTrackingState } = this.router;
-
-    const { category } = this.discovery;
+    const { currentRouteName} = this.router;
+    const { category  } = this.discovery;
 
     let categoryName = ""
     let categoryDescription = ""
     if (category) {
-      // console.log('category:', category);
       categoryName = category.name
       categoryDescription = category.description
     }
 
-
-    if (currentRouteName === `discovery.${defaultHomepage()}`) {
+    if (currentRouteName === `discovery.${defaultHomepage()}` || currentRouteName.startsWith("topic") ) {
       return { homepageHero: true }
     }
 
-    if (currentRouteName === "discovery.category" || currentRouteName.startsWith("topic")) {
+    if (currentRouteName === "discovery.category" ) {
       return { dynamicHero: true, title: categoryName, subtitle: categoryDescription }
     }
-
     return { emptyHero: true }
   }
 
-
-}
+} 
